74764d12e2a4d300fa2e14f11cdee39ce2ad3d66
syrehgar <syrehgar@gmail.com>
2017-09-26 15:05:02 +0800

    Add Swift file

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+str += "!"
+print(str)
+
+/* 第一章：熟悉Xcode与Playground
+ * 难易程度：简单
+ */
+var name = "Song"
+print(name)
+//print带有换行占位符
+
+/* 第二章：类型、变量与常量
+ * 难易程度：简单
+ */
+//Swift用类型推断来确定变量的类型
+
+
+
