d8d6f5656f2bb0319933c0e5e38845f0a8cd93b1
sygriffin <786281584@qq.com>
2018-07-09 11:08:26 +0800

    finish classfunc

--- MonsterTown/MonsterTown/Zombie.swift
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -15,5 +15,19 @@ class Zombie: Monster {
         town?.changePopulation(by: -10)
         super.terrorizeTown()
     }
+    //类的类型方法要用class关键字标记
+    //不想被子类重写就用static
+    //利用final也可以代替
+    /*static&final*/ class func makeSpookeyNoise() -> String {
+        return "Brains..."
+    }
     
 }
+
+//子类可以重写父类方法
+class GiantZombie : Zombie {
+    override class func makeSpookeyNoise () -> String {
+        return "ROAR!!!"
+    }
+}
+//不想让子类继承某个类方法 -- 用static 见上
--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -28,7 +28,20 @@ fredTheZombie.town?.printDescription()
 
 ///深入学习--类型方法-->对定义类型本身调用的方法
 //1.对于值类型，要声明类型方法需要使用static关键字
+struct Square {
+    static func numberOfSides() -> Int {
+        return 4
+    }
+}
+//2.类的类型方法要用class关键字标记
+//详见Zombie.swift
+//直接调用类方法
+let spookeyNoise = Zombie.makeSpookeyNoise()
+//3.不想让子类继承某个类方法
+//详见Zombie.swift
 
+//类方法既不能调用实例方法，也不能使用实例属性
 
+///深入学习 -- mutating方法
 
 
