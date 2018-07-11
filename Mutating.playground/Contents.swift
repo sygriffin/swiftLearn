ad01c4518a196f0e93fa93c2f29aa2feeabd038a
sygriffin <786281584@qq.com>
2018-07-11 16:06:03 +0800

    finish mutating

--- Mutating.playground/Contents.swift
+++ Mutating.playground/Contents.swift
@@ -10,6 +10,44 @@ func greet(name:String, withGreeting greeting:String) -> String {
 
 let personalGreeting = greet(name: "Matt", withGreeting: "Hello")
 
+func greeting(forName name:String) -> (String) -> String {
+    func greeting(_ greeting:String) -> String {
+        return "\(greeting)\(name)"
+    }
+    return greeting
+}
+
+let greetMattWith = greeting(forName: "Matt")
+let mattGreeting = greetMattWith("hello,")
+print(mattGreeting)
+
+//更紧凑的函数 -- 运用函数闭包
+func greeting1(_ greeting:String) -> (String) -> String {
+    return { (name:String) -> String in
+        return "\(greeting)\(name)"
+    }
+}
+let friendlyGreetingFor = greeting1("Hello,")
+let mattGreeting1 = friendlyGreetingFor("Matt")
+print(mattGreeting1)
+
+struct Person {
+    var firstName = "Matt"
+    var lastName = "Mathis"
+    
+    mutating func changeTo(firstName:String, lastName:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+var p = Person()
+//let changName = Person.changeTo
+//let changNameTo = changName(&p)
+//changNameTo("John","Lennon")
+p.firstName
+p.changeTo(firstName: "yang", lastName: "song")
+p.firstName
 
 
 
