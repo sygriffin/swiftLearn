7a026e20e66ced3838f16b5252593e97f8f0970b
sygriffin <786281584@qq.com>
2018-11-29 14:24:02 +0800

    learn about swift4 Notify

---
+++ HelloSwift/HelloSwift/FindVC.swift
@@ -0,0 +1,58 @@
+//
+//  FindVC.swift
+//  HelloSwift
+//
+//  Created by songyang on 2018/11/29.
+//  Copyright © 2018年 songyang. All rights reserved.
+//
+
+import UIKit
+
+class FindVC: UIViewController, UITextFieldDelegate{
+
+    @IBOutlet weak var headerTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(FindVC.keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(FindVC.keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        NSLog("键盘弹出")
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        NSLog("键盘收回")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
