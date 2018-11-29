03b79d410ded3aca66550a681cf039de559e1c73
sygriffin <786281584@qq.com>
2018-11-29 15:11:40 +0800

    About swift button

--- SwiftPureCode/SwiftPureCode/MainVC.swift
+++ SwiftPureCode/SwiftPureCode/MainVC.swift
@@ -10,13 +10,27 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    var button: UIButton?
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.green
+        
+        self.button = UIButton(frame:CGRect(x: 20, y: 100, width: self.view.frame.width - 40, height: 44))
+        button?.setTitle("执行按钮", for:.normal)
+        button?.setTitleColor(UIColor.black, for:.normal)
+        button?.backgroundColor = UIColor.white
+        button?.addTarget(self, action: #selector(MainVC.touchButton), for:.touchUpInside)
+        self.view.addSubview(button!)
+        
         // Do any additional setup after loading the view.
     }
     
+    @objc func touchButton() {
+        NSLog("点击了执行按钮")
+    }
 
     /*
     // MARK: - Navigation
