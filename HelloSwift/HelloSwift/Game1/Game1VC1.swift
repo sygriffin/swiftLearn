021809261f4f76cc4c4f628c506cbae47d90cd81
sygriffin <786281584@qq.com>
2018-12-25 16:40:55 +0800

    all changes

---
+++ HelloSwift/HelloSwift/Game1/Game1VC1.swift
@@ -0,0 +1,30 @@
+//
+//  Game1VC1.swift
+//  HelloSwift
+//
+//  Created by songyang on 2018/12/10.
+//  Copyright © 2018年 songyang. All rights reserved.
+//
+
+import UIKit
+
+class Game1VC1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HelloSwift/HelloSwift/Game1/Game1_RollingVC.swift
@@ -0,0 +1,30 @@
+//
+//  Game1_RollingVC.swift
+//  HelloSwift
+//
+//  Created by songyang on 2018/12/10.
+//  Copyright © 2018年 songyang. All rights reserved.
+//
+
+import UIKit
+
+class Game1_RollingVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
