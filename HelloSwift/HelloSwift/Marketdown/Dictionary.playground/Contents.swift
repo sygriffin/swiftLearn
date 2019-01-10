462141569a472cd68bd6089b579b1c57955ac732
sygriffin <786281584@qq.com>
2019-01-10 15:00:33 +0800

    Go on Dict

--- HelloSwift/HelloSwift/Marketdown/Dictionary.playground/Contents.swift
+++ HelloSwift/HelloSwift/Marketdown/Dictionary.playground/Contents.swift
@@ -51,10 +51,80 @@ record10.forEach{
 //字典是无序的集合 -- 若想遍历出有序的，则要先进行排序
 
 record11.keys.sorted().forEach {
-    print("\($0) -- \(record11[$0]!)")
+    print("\($0) -- \(record11[$0] ?? .text("nil"))")
 }
 
 
+//常用的字典Extension
+let defaultRecord : [String: RecordType] = [
+    "uid": .number(0),
+    "exp": .number(100),
+    "favourite": .bool(false),
+    "title": .text("")
+]
 
+var template = defaultRecord
 
+//差异内容
+let record11Patch: [String: RecordType] = [
+    "uid": .number(11),
+    "title": .text("Common Extension")
+]
 
+//template.merge(record11Patch, uniquingKeysWith: <#(RecordType, RecordType) throws -> RecordType#>)
+
+extension Dictionary {
+    //dictionary -- struct类型，并且要修改merge，s所以使用mutating
+    mutating func merge<S:Sequence>(_ sequence: S)
+        where S.Iterator.Element == (key: Key, value:Value) {
+            sequence.forEach{self[$0] = $1}
+    }
+    
+    init<S:Sequence>(_ sequence: S)
+        where S.Iterator.Element == (key: Key, value:Value) {
+            
+            self = [:]
+            self.merge(sequence)
+    }
+    
+    func mapValue<T>(_ trasform: (Value) -> T) -> [Key: T] {
+        return Dictionary<Key, T>(map {($0, trasform($1))})
+    }
+}
+
+template.merge(record11Patch)
+
+template
+
+let record10Patch:[(key: String, value:RecordType)] = [
+    
+    (key: "uid", value: .number(10)),
+    (key: "title", value: .text("Common dict ext"))
+
+]
+
+var template1 = defaultRecord
+template1.merge(record10Patch)
+template1
+
+let record11PatchDict = Dictionary(record11Patch)
+print(record11PatchDict)
+print("+++++++++++++++++++++++")
+print(record11PatchDict.map{$1})
+
+//全部都转成String
+let record111 = record11PatchDict.mapValue{ record -> String in
+    
+    switch record {
+    case .text(let title):
+        return title
+    case .number(let exp):
+        return String(exp)
+    case .bool(let favourite):
+        return String(favourite)
+    }
+    
+    
+}
+
+print(record111)
