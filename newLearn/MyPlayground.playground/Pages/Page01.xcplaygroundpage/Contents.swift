8fcb22f2e2f52a1d7e71cd2f419f2f0d9e6d884e
songyang <songyang1@akulaku.com>
2020-09-23 16:27:16 +0800

    rebase swift and new rx

---
+++ newLearn/MyPlayground.playground/Pages/Page01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+
+
+import Foundation
+
+var str = "Hello, playground"
+
+/* 支持嵌套
+ /*  */
+ 
+ //: markup语法
+ #
+ ##
+ ###
+ -
+ -
+ [](link)
+ 与markdown类似
+ 仅仅支持playground中支持
+ > This is a note
+ 
+ */
+
+//下一页
+//: [Next](@next)
+
+let a : Int
+//编译时 不确定这个值
+
+//值类型 与 引用类型
+//无论是结构体 还是常量 编译器都是进行过优化的
+//结构体 可扩展性也是很强的
+
+let intMax = Int.max
+//浮点数十进制写法 e2 e-2
+
+
+//巧用tuple - 元组
