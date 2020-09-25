65c21680e65b31d4f0b359bcdffefdf966bbf013
songyang <songyang1@akulaku.com>
2020-09-25 18:51:16 +0800

    new proj

---
+++ newLearn/swiftLearn/swiftLearn/Prefix.swift
@@ -0,0 +1,12 @@
+//
+//  Prefix.swift
+//  swiftLearn
+//
+//  Created by sy on 2020/9/25.
+//  Copyright © 2020 sy. All rights reserved.
+//
+
+import Foundation
+
+@_exported import Swift_Log
+@_exported import NSLogger
---
+++ newLearn/swiftLearn/swiftLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftLearn
+//
+//  Created by sy on 2020/9/25.
+//  Copyright © 2020 sy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
