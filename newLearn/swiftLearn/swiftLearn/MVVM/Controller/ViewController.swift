d041f0f02c7a546e3ce4c01295ca7482df74f670
songyang <songyang1@akulaku.com>
2020-11-09 12:22:19 +0800

    order

--- newLearn/swiftLearn/swiftLearn/MVVM/Controller/ViewController.swift
+++ newLearn/swiftLearn/swiftLearn/MVVM/Controller/ViewController.swift
@@ -57,6 +57,24 @@ class ViewController: BaseViewController {
         //退出页面时 移除
         disposable.dispose()
         */
+        // 创建序列
+        
+        // 最直接的就是调用create
+//        Observable.create(<#T##subscribe: (AnyObserver<_>) -> Disposable##(AnyObserver<_>) -> Disposable#>)
+//        .onNext -> 产生一个元素
+//        .onCompleted -> 元素已经产生完了
+        
+        // Event 事件 - 合理利用这些Event来实现业务逻辑
+        
+        
+        // 特征序列 - Single Completable Maybe Driver Signal
+        
+        // Single 只能发出一个元素 要么产生一个 error 事件 -> 与observable创建很相似 不会共享附加作用 => 对Observable调用 .asSingle()方法，将其转换为Single
+        // Completable -> 只能产生一个 completed事件或者error事件；不会共享附加作用；Completable 是 Observable 的另外一个版本。
+        // Maybe 介于Single与Completable之间
+        // Driver 精心准备的特征序列 - 主要为了简化UI层的代码 -> 不产生error、一定在主线程监听、共享附加作用
+        
+        
         
         
         
